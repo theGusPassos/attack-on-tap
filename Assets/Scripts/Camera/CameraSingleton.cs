26dacb25e63c7b7ac57cd1a8c680eb3aee5e1e27
Gustavo Passos <passosgu@sempreit.com.br>
2017-10-14 18:41:26 -0300

    camera setup

---
+++ Assets/Scripts/Camera/CameraSingleton.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace AttackOnTap.Camera
+{
+    public class CameraSingleton : MonoBehaviour
+    {
+        private bool created = false;
+
+        private void Awake()
+        {
+            if (!created)
+                created = true;
+            else
+                Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Camera/PixelPerfectCamera.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace AttackOnTap.Camera
+{
+    public class PixelPerfectCamera : MonoBehaviour
+    {
+        public int pixelsPerUnit = 6;
+
+        private UnityEngine.Camera _camera;
+
+        private void Awake()
+        {
+            _camera = GetComponent<UnityEngine.Camera>();
+
+            _camera.orthographicSize = Screen.height / 2f /pixelsPerUnit;
+            print(_camera.orthographicSize );
+        }
+    }
+}
